e9849ea1f8a76956f22595ae6792e2bd809eb01b
Marcela Carvalho <65089522+CelaCarvalho@users.noreply.github.com>
2020-06-02 20:39:23 -0300

    Add files via upload

---
+++ scripts/intro.js
@@ -0,0 +1,76 @@
+//comentários
+//document.write ("Hello")
+//variáveis e tipos de dados
+//var myvar = "Hi"
+//myvar = "Hello"
+//document.write(myvar)
+//As variáveis servem para registar e/ou guardar em memória para usar mais tarde
+//const myconst = "Hello"
+//document.write(myconst+myvar)
+
+
+//string
+//const s1 = "Isso é uma string"
+//const s2 = 'isso é uma string'
+//const s3 = `isso é uma string`
+
+//document.write(s1)
+
+
+//number
+//const n1 = 1
+//const n2 = 12
+//document.write(n1 + n2)
+
+//boolean (recebe dois valores true ou false)
+//const b1 = true
+//const b2 = false
+
+//Tudo o que foi visto é um tipo de dado
+//Como estrtuturar os dados?
+//objeto (todos possuem propriedades e funcionalidades)
+//Exemplo
+//const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  salteiro: true,
+//  correr(){  
+//    document.write("executar uma grande função correr")  
+//  }
+//}
+//pessoa.correr()
+
+
+//array - vetor []
+//const colecaodebolinhas = ["blue","green",1,{name:"my name"}]
+
+//document.write(colecaodebolinhas[3].name)
+
+
+//funções - funcionalidades-atalhos-reuso de códigos
+//Registrar função
+//function sayMyname(name){
+//  document.write(name)
+//}
+//Exeutar
+//sayMyname("Marcela")
+//sayMyname("Thaynan")
+
+
+//Condicional
+//const notaFinal = 7
+//if (notaFinal < 5){
+//  document.write("Reprovado")
+//} else { 
+//  document.write("Aprovado")
+//}
+
+//looping repetições
+
+//for (i = 0; i < 10; i++) {
+//  document.write("<p>alo</p>")
+//}
+//for (i =0;i < 10;i++) {
+//  document.write(`<p>${i}</p>`)
+//}
+
